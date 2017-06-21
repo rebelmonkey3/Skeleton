4c04b3f85531cfbb1a134aec189b0ba360c604e8
Blake Weisgarber <rebelmonkey3@gmail.com>
2017-06-20 17:58:58 -0700

    work in progress

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,7 +2,28 @@ import React from "react";
 
 // very basic component to get started
 const Main = () => (
-  <h1>Hello World!</h1>
-);
+  render: function() {
+
+    return (
+
+      <div className="form">
+        <div>
+          <h2><What is your Quote?</strong></h2>
+          <p>
+            <a href="#/Child1" className="btn btn-primary btn-lg">Show Child #1</a>
+            <a href="#/Child2" className="btn btn-primary btn-lg">Show Child #2</a>
+          </p>
+        </div>
+
+        <div className="row">
+
+          {/* This code will dump the correct Child Component */}
+          {this.props.children}
+
+        </div>
+ 		<button id="update"> Darth Vadar invades! </button>
+      </div>
+    );
+ });
 
 export default Main;
